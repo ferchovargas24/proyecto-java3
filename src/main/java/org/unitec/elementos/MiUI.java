e6251ddef446c50ed862839ec995185b08c9781d
T-107 <T-107@PC183>
2017-09-15 21:26:48 -0500

    java3 - 2

--- src/main/java/org/unitec/elementos/MiUI.java
+++ src/main/java/org/unitec/elementos/MiUI.java
@@ -10,6 +10,7 @@ import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ValoTheme;
@@ -26,11 +27,17 @@ public class MiUI extends UI{
         VerticalLayout vl = new VerticalLayout();
 
         //Una etiqueta vaadin
-        Label etiqueta = new Label("Hola mundo vaadin");
+        Label etiqueta = new Label("Hola mundo vaadin Fernando");
         //Un boton 
         Button boton = new Button("Apachurrame!");
         //Ponemos un estilo al boton
         boton.addStyleName(ValoTheme.BUTTON_DANGER);
+        //Vamos a manejar un evento de boton del tipo click
+        boton.addClickListener(/*nombre del evento*/algo->{
+            //Aqui ponemos la funcion
+            Notification.show("Yo soy una notificación", Notification.Type.WARNING_MESSAGE);
+        });
+        
         vl.addComponent(etiqueta);
         vl.addComponent(boton);
         setContent(vl);
