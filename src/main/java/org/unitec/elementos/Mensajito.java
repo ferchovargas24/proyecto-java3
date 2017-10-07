db0cd644e289605243d72aed1845df79bdc6895d
T-107 <T-107@PC193>
2017-10-06 21:37:13 -0500

    java3- sql2

--- src/main/java/org/unitec/elementos/Mensajito.java
+++ src/main/java/org/unitec/elementos/Mensajito.java
@@ -2,8 +2,61 @@
 package org.unitec.elementos;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class Mensajito {
+    
+    @Id
+    @GeneratedValue//se pone cuando este id es un autoincrement        
+    private Integer id;
+    
+    private String titulo;
+    
+    private String cuerpo;
 
+    public Mensajito() {
+    }
+
+    public Mensajito(String titulo, String cuerpo) {
+        this.titulo = titulo;
+        this.cuerpo = cuerpo;
+    }
+
+    public Mensajito(Integer id, String titulo, String cuerpo) {
+        this.id = id;
+        this.titulo = titulo;
+        this.cuerpo = cuerpo;
+    }
+
+    public Mensajito(Integer id) {
+        this.id = id;
+    }
+    
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getCuerpo() {
+        return cuerpo;
+    }
+
+    public void setCuerpo(String cuerpo) {
+        this.cuerpo = cuerpo;
+    }
+    
 }
